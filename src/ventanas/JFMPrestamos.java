4b194e25c5257224bd592e0a554c45ca61ddd8ae
Mrale1058 <144624425+Mrale1058@users.noreply.github.com>
2023-12-07 19:13:40 -0400

    Update JFMPrestamos.java

--- src/ventanas/JFMPrestamos.java
+++ src/ventanas/JFMPrestamos.java
@@ -5,12 +5,17 @@ import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import java.io.BufferedWriter;
+
+import java.io.FileWriter;
+
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.swing.JOptionPane;
 public class JFMPrestamos extends JFrame {
     private JTable tablePrestamo;
@@ -31,27 +36,28 @@ public class JFMPrestamos extends JFrame {
         	new Object[][] {
         	},
         	new String[] {
-        		"CI", "ISBM", "Estado", "Fecha prestada", "Fecha devolucion", "Multa"
+        		"CI", "ISBM", "Estado", "Fecha prestada", "Fecha devolucion"
         	}
         ));
         scrollPane.setViewportView(tablePrestamo);
 
         JLabel lblPrestamo = new JLabel("Prestamo");
-        lblPrestamo.setFont(new Font("Tahoma", Font.BOLD, 20));
         lblPrestamo.setBounds(51, 64, 101, 14);
+        lblPrestamo.setFont(new Font("Tahoma", Font.BOLD, 20));
         getContentPane().add(lblPrestamo);
 
         JButton btnEditar = new JButton("Editar");
+        btnEditar.setBounds(247, 581, 150, 23);
         btnEditar.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
             	boolean editable = !tablePrestamo.isEditing();
                 tablePrestamo.setEnabled(editable);
             }
         });
-        btnEditar.setBounds(247, 581, 150, 23);
         getContentPane().add(btnEditar);
 
         JButton btnEliminar = new JButton("Eliminar");
+        btnEliminar.setBounds(637, 581, 150, 23);
         btnEliminar.addActionListener(new ActionListener() {
         	public void actionPerformed(ActionEvent e) {
         		int filaSeleccionada = tablePrestamo.getSelectedRow();
@@ -63,19 +69,19 @@ public class JFMPrestamos extends JFrame {
                 }
         	}
         });
-        btnEliminar.setBounds(637, 581, 150, 23);
         getContentPane().add(btnEliminar);
 
         JButton btnActualizar = new JButton("Actualizar");
+        btnActualizar.setBounds(452, 581, 150, 23);
         btnActualizar.addActionListener(new ActionListener() {
         	public void actionPerformed(ActionEvent e) {
         		tablePrestamo.setEnabled(false);
         	}
         });
-        btnActualizar.setBounds(452, 581, 150, 23);
         getContentPane().add(btnActualizar);
 
         JButton btnAtras = new JButton("Atras");
+        btnAtras.setBounds(847, 610, 89, 23);
         btnAtras.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
             	JFMPantallaInicio pantallaInicio = new JFMPantallaInicio();
@@ -83,10 +89,10 @@ public class JFMPrestamos extends JFrame {
                 dispose();
             }
         });
-        btnAtras.setBounds(847, 610, 89, 23);
         getContentPane().add(btnAtras);
         
         JButton btnNewButton = new JButton("Agregar Prestamo");
+        btnNewButton.setBounds(48, 581, 154, 23);
         btnNewButton.addActionListener(new ActionListener() {
         	public void actionPerformed(ActionEvent e) {
                     JFMAgregarPrestamo frame = new JFMAgregarPrestamo();
@@ -94,67 +100,62 @@ public class JFMPrestamos extends JFrame {
                     dispose();
         	}
         });
-        btnNewButton.setBounds(48, 581, 154, 23);
         getContentPane().add(btnNewButton);
-        
         JButton btnNewButton_1 = new JButton("Mostrar Todo");
-        btnNewButton_1.setBounds(711, 64, 129, 42);
+        btnNewButton_1.setBounds(761, 64, 145, 36);
         getContentPane().add(btnNewButton_1);
         
         btnNewButton_1.addActionListener(new ActionListener() {
         	public void actionPerformed(ActionEvent e) {
         		//cargarDatosDesdeArchivo("Prestamos.txt");
+        		cargarDatosDesdeArchivo("Prestamos.txt");
         	}
         });
-        
-        btnNewButton_1.setBounds(767, 138, 145, 36);
         getContentPane().add(btnNewButton_1);
+        cargarDatosDesdeArchivo("Prestamos.txt");
         
-        //cargarDatosDesdeArchivo("Prestamos.txt");
     }
-    private String extraerValor(String linea, String etiqueta) {
-        return linea.substring(etiqueta.length()).trim();
-    }
-    /*
     private void cargarDatosDesdeArchivo(String nombreArchivo) {
-        List<Prestamos> listaPrestamos = obtenerListaPrestamosDesdeArchivo(nombreArchivo);
-
         DefaultTableModel modelo = (DefaultTableModel) tablePrestamo.getModel();
         modelo.setRowCount(0); // Limpiar la tabla antes de cargar nuevos datos
 
-        for (Prestamos prestamo : listaPrestamos) {
-            Object[] fila = new Object[modelo.getColumnCount()];
-            fila[0] = prestamo.getCI(); // CI
-            fila[1] = prestamo.getIBMS(); // IBMS
-            fila[2] = prestamo.getFechaprestamo(); // Fechaprestamo
-            fila[3] = prestamo.getFechadevolucion(); // Fechadevolucion
-            fila[4] = prestamo.getestado; // Multas pendientes (no hay información en el archivo)
-
-            modelo.addRow(fila);
-        }
-    }*/
-    private List<Prestamos> obtenerListaPrestamosDesdeArchivo(String nombreArchivo) {
-        List<Prestamos> listaPrestamo = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new FileReader(nombreArchivo))) {
             String linea;
             while ((linea = reader.readLine()) != null) {
-                if (linea.startsWith("CI: ")) {
-                    int ci = Integer.parseInt(extraerValor(linea, "CI: "));
-                    String fechaprestamo = extraerValor(reader.readLine(), "Fecha Prestamo: ");
-                    String fechadevolucion = extraerValor(reader.readLine(), "Fecha Devolucion: ");
-                    String estado = extraerValor(reader.readLine(), "Estado: ");
+                // Procesar cada línea del archivo para obtener los datos
+                String ciStr = extraerValor(linea, "CI: ");
+                String isbnStr = extraerValor(reader.readLine(), "Codigo Libro: ");
+                String fechaPrestamo = extraerValor(reader.readLine(), "Fecha prestamo: ");
+                String fechaDevolucion = extraerValor(reader.readLine(), "Fecha devolucion: ");
+                String estado = extraerValor(reader.readLine(), "Estado: ");
 
-                    Prestamos prestamo = new Prestamos(ci, fechaprestamo, fechadevolucion,estado);
-                    listaPrestamo.add(prestamo);
-
-                    reader.readLine(); // Leer la línea de separación
-                }
+                modelo.addRow(new Object[]{ciStr, isbnStr, estado, fechaPrestamo, fechaDevolucion, ""});
+                
+                reader.readLine(); // Leer la línea de separación
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return listaPrestamo;
     }
+    
+
+    private String extraerValor(String linea, String etiqueta) {
+        if (linea.startsWith(etiqueta)) {
+            return linea.substring(etiqueta.length()).trim();
+        } else {
+            return "";
+        }
+    }
+
+    private int parseIntegerValue(String value) {
+        try {
+            return Integer.parseInt(value);
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+    
+
     public static void main(String[] args) {
         EventQueue.invokeLater(() -> {
             try {
