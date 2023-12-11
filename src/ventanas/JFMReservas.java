922bc406edbb920f20bcf8f3fe8063788e7dc6f8
Jhuly1215 <jhuliannnavitoria@gmail.com>
2023-12-10 20:26:13 -0400

    Reservas a medias UnU

--- src/ventanas/JFMReservas.java
+++ src/ventanas/JFMReservas.java
@@ -5,11 +5,29 @@ import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
 import javax.swing.JOptionPane;
 public class JFMReservas extends JFrame {
-    private JTable tablePrestamo;
-
+    private JTable tableReservas;
+    private LinkedList<Reserva> listaReservas;
+    private List<Usuario> usuariosRegistrados;
+    private List<Libro> librosRegistrados;
+    
+    
     public JFMReservas() {
+    	usuariosRegistrados = obtenerListaUsuariosDesdeArchivo("Usuarios.txt");
+        librosRegistrados = obtenerListaLibrosDesdeArchivo("LibrosGuardados.txt");
+        
         getContentPane().setBackground(new Color(62, 95, 138));
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setBounds(100, 100, 1000, 700);
@@ -19,16 +37,20 @@ public class JFMReservas extends JFrame {
         scrollPane.setBounds(48, 122, 800, 400);
         getContentPane().add(scrollPane);
 
-        tablePrestamo = new JTable();
-        tablePrestamo.setEnabled(false);
-        tablePrestamo.setModel(new DefaultTableModel(
+        tableReservas = new JTable();
+        tableReservas.setFocusable(false);
+        //tableReservas.setEnabled(false);
+        tableReservas.setRowSelectionAllowed(true);
+        tableReservas.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        
+        tableReservas.setModel(new DefaultTableModel(
                 new Object[][] {
                 },
                 new String[] {
-                        "Usuario", "Libro", "Fecha de reserva"
+                        "Cod Reserva","Usuario", "Libro", "Fecha de reserva"
                 }
         ));
-        scrollPane.setViewportView(tablePrestamo);
+        scrollPane.setViewportView(tableReservas);
 
         JLabel lblPrestamo = new JLabel("Reservas");
         lblPrestamo.setFont(new Font("Tahoma", Font.BOLD, 20));
@@ -41,43 +63,45 @@ public class JFMReservas extends JFrame {
         btnAgregar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                agregarFila();
+                JFMReservasRegistrar frame=new JFMReservasRegistrar();
+                frame.setVisible(true);
+                dispose();
             }
         });
-
-        JButton btnEditar = new JButton("Editar");
-        btnEditar.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                boolean editable = !tablePrestamo.isEditing();
-                tablePrestamo.setEnabled(editable);
-            }
-        });
-        btnEditar.setBounds(247, 581, 150, 23);
-        getContentPane().add(btnEditar);
 
         JButton btnEliminar = new JButton("Eliminar");
         btnEliminar.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                int filaSeleccionada = tablePrestamo.getSelectedRow();
+                int filaSeleccionada = tableReservas.getSelectedRow();
                 if (filaSeleccionada >= 0) {
-                    DefaultTableModel model = (DefaultTableModel) tablePrestamo.getModel();
-                    model.removeRow(filaSeleccionada);
+                    int confirmacion = JOptionPane.showConfirmDialog(
+                            null,
+                            "¿Está seguro de eliminar esta reserva?",
+                            "Confirmación de eliminación",
+                            JOptionPane.YES_NO_OPTION);
+
+                    if (confirmacion == JOptionPane.YES_OPTION) {
+                        // Obtener el PrID de la fila seleccionada
+                        int prId = (int) tableReservas.getValueAt(filaSeleccionada, 0);
+
+                        // Llamar al método eliminarReserva
+                        eliminarReserva(prId);
+
+                        // Actualizar la visualización de la tabla
+                        cargarReservasDesdeArchivo("Reservas.txt");
+                    }
                 } else {
-                    JOptionPane.showInternalConfirmDialog(null, "Selecciona una fila para eliminar", "Error", JOptionPane.ERROR_MESSAGE);
+                    JOptionPane.showMessageDialog(
+                            null,
+                            "Seleccione una fila para eliminar",
+                            "Error",
+                            JOptionPane.ERROR_MESSAGE);
                 }
             }
         });
+
         btnEliminar.setBounds(637, 581, 150, 23);
         getContentPane().add(btnEliminar);
-
-        JButton btnActualizar = new JButton("Actualizar");
-        btnActualizar.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                tablePrestamo.setEnabled(false);
-            }
-        });
-        btnActualizar.setBounds(452, 581, 150, 23);
-        getContentPane().add(btnActualizar);
 
         JButton btnAtras = new JButton("Atras");
         btnAtras.addActionListener(new ActionListener() {
@@ -89,13 +113,153 @@ public class JFMReservas extends JFrame {
         });
         btnAtras.setBounds(860, 610, 89, 23);
         getContentPane().add(btnAtras);
+        
+        cargarReservasDesdeArchivo("Reservas.txt");
+    }
+    private void eliminarReserva(int prId) {
+        // Obtener la lista de reservas actual
+        List<Reserva> listaReservas = obtenerListaReservasDesdeArchivo("Reservas.txt");
+
+        // Buscar la reserva con el ID proporcionado
+        Optional<Reserva> reservaAEliminar = listaReservas.stream()
+                .filter(reserva -> reserva.getCodigoReserva() == prId)
+                .findFirst();
+
+        // Si se encuentra la reserva, eliminarla de la lista
+        reservaAEliminar.ifPresent(reserva -> listaReservas.remove(reserva));
+
+        // Guardar la lista actualizada en el archivo
+        guardarListaReservasEnArchivo(listaReservas, "Reservas.txt");
     }
 
-    private void agregarFila() {
-        DefaultTableModel model = (DefaultTableModel) tablePrestamo.getModel();
-        model.addRow(new Object[]{"Nuevo CI", "Nuevo ISBM", "Nuevo Estado", "Nueva Fecha prestada", "Nueva Fecha devolucion", "Nueva Multa"});
+    private void guardarListaReservasEnArchivo(List<Reserva> listaReservas, String nombreArchivo) {
+        // Guardar la lista de reservas en el archivo
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(nombreArchivo))) {
+            for (Reserva reserva : listaReservas) {
+                writer.write("PrID: " + reserva.getCodigoReserva() + "\n");
+                writer.write("CI: " + reserva.getUsuario().getCI() + "\n");
+                writer.write("Codigo Libro: " + reserva.getLibro().getIsbn() + "\n");
+                writer.write("Fecha reserva: " + reserva.getFechaReserva() + "\n");
+                writer.write("------------------------------\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
+    private String extraerValor(String linea, String etiqueta) {
+        return linea.substring(etiqueta.length()).trim();
+    }
+    private void cargarReservasDesdeArchivo(String nombreArchivo) {
+        listaReservas = obtenerListaReservasDesdeArchivo(nombreArchivo);
+
+        DefaultTableModel modelo = (DefaultTableModel) tableReservas.getModel();
+        modelo.setRowCount(0); // Limpiar la tabla antes de cargar nuevos datos
+
+        for (Reserva reserva : listaReservas) {
+            Object[] fila = new Object[modelo.getColumnCount()];
+            fila[0]=reserva.getCodigoReserva();
+            fila[1] = reserva.getUsuario(); // CI
+            fila[2] = reserva.getLibro(); // Nombre
+            fila[3] = reserva.getFechaReserva(); // Celular
+         
+
+            modelo.addRow(fila);
+        }
+    }
+    private LinkedList<Reserva> obtenerListaReservasDesdeArchivo(String nombreArchivo) {
+        LinkedList<Reserva> listaReservas = new LinkedList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(nombreArchivo))) {
+            String linea;
+            while ((linea = reader.readLine()) != null) {
+                if (linea.startsWith("PrID: ")) {
+                    int prId = Integer.parseInt(extraerValor(linea, "PrID: "));
+                    int ci = Integer.parseInt(extraerValor(reader.readLine(), "CI: "));
+                    String codigoLibro = extraerValor(reader.readLine(), "Codigo Libro: ");
+                    String fechaReserva = extraerValor(reader.readLine(), "Fecha reserva: ");
+
+                    // Crear un objeto Reserva y agregarlo a la lista
+                    Usuario usuario = buscarUsuarioPorCI(ci);
+                    Libro libro = buscarLibroPorISBN(codigoLibro);
+                    if (usuario != null && libro != null) {
+                        Reserva reserva = new Reserva(prId, usuario, libro, fechaReserva);
+                        listaReservas.add(reserva);
+                    }
+
+                    reader.readLine(); // Leer la línea de separación
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return listaReservas;
+    }
+    private Usuario buscarUsuarioPorCI(int ci) {
+        for (Usuario usuario : usuariosRegistrados) {
+            if (usuario.getCI() == ci) {
+                return usuario;
+            }
+        }
+        return null; // No se encontró el usuario
+    }
+
+    // Método para buscar un libro por código
+    private Libro buscarLibroPorISBN(String codigoLibro) {
+        for (Libro libro : librosRegistrados) {
+            if (libro.getIsbn().equals(codigoLibro)) {
+                return libro;
+            }
+        }
+        return null; // No se encontró el libro
+    }
+    
+    private List<Usuario> obtenerListaUsuariosDesdeArchivo(String nombreArchivo) {
+        List<Usuario> listaUsuario = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(nombreArchivo))) {
+            String linea;
+            while ((linea = reader.readLine()) != null) {
+                if (linea.startsWith("CI: ")) {
+                    int ci = Integer.parseInt(extraerValor(linea, "CI: "));
+                    String nombre = extraerValor(reader.readLine(), "Nombre: ");
+                    int celular = Integer.parseInt(extraerValor(reader.readLine(), "Celular: "));
+
+                    Usuario usuario = new Usuario(ci, nombre, celular);
+                    listaUsuario.add(usuario);
+
+                    reader.readLine(); // Leer la línea de separación
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return listaUsuario;
+    }
+
+    // Método para cargar libros desde el archivo
+    private List<Libro> obtenerListaLibrosDesdeArchivo(String nombreArchivo) {
+        List<Libro> listaLibros = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(nombreArchivo))) {
+            String linea;
+            while ((linea = reader.readLine()) != null) {
+
+                String isbn = extraerValor(linea, "ISBN:");
+                String titulo = extraerValor(reader.readLine(), "Título:");
+                String autor = extraerValor(reader.readLine(), "Autor:");
+                String genero = extraerValor(reader.readLine(), "Género:");
+                int anioPublicacion = Integer.parseInt(extraerValor(reader.readLine(), "Año de Publicación:"));
+                int cantidad = Integer.parseInt(extraerValor(reader.readLine(), "Cantidad:"));
+                String rutaImagen = extraerValor(reader.readLine(), "Ruta de la Imagen:");
+
+                Libro libro = new Libro(isbn, titulo, autor, genero, anioPublicacion, cantidad, rutaImagen);
+                listaLibros.add(libro);
+                reader.readLine();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return listaLibros;
+    }
     public static void main(String[] args) {
         EventQueue.invokeLater(() -> {
             try {
---
+++ src/ventanas/JFMReservasRegistrar.java
@@ -0,0 +1,276 @@
+package ventanas;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+public class JFMReservasRegistrar extends JFrame {
+    private static final long serialVersionUID = 1L;
+    private JPanel contentPane;
+    private JTextField txCI;
+    private JTextField txCodigoLibro;
+    private JTextField txFechareserva;
+    private List<Usuario> usuariosRegistrados;
+    private List<Libro> librosRegistrados;
+
+    
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                	
+                	JFMReservasRegistrar frame = new JFMReservasRegistrar();
+                	frame.setVisible(true);
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the frame.
+     */
+    public JFMReservasRegistrar() {
+    	
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 1000, 700);
+        contentPane = new JPanel();
+        contentPane.setBackground(new Color(62, 95, 138));
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+
+        usuariosRegistrados = obtenerListaUsuariosDesdeArchivo("Usuarios.txt");
+        librosRegistrados = obtenerListaLibrosDesdeArchivo("LibrosGuardados.txt");
+
+        JPanel panel_1 = new JPanel();
+        panel_1.setLayout(null);
+        panel_1.setBounds(221, 131, 567, 396);
+        contentPane.add(panel_1);
+
+        JLabel lblNewLabel = new JLabel("AÑADIR RESERVA");
+        lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 16));
+        lblNewLabel.setBounds(221, 45, 201, 31);
+        panel_1.add(lblNewLabel);
+
+        txCI = new JTextField();
+        txCI.setBounds(327, 126, 178, 20);
+        panel_1.add(txCI);
+        txCI.setColumns(10);
+
+        txCodigoLibro = new JTextField();
+        txCodigoLibro.setBounds(327, 171, 178, 20);
+        panel_1.add(txCodigoLibro);
+        txCodigoLibro.setColumns(10);
+
+        txFechareserva = new JTextField();
+        txFechareserva.setBounds(327, 217, 178, 20);
+        panel_1.add(txFechareserva);
+        txFechareserva.setColumns(10);
+
+        JLabel lblCI = new JLabel("Carnet de Identidad:");
+        lblCI.setFont(new Font("Rockwell", Font.PLAIN, 18));
+        lblCI.setBounds(125, 127, 178, 14);
+        panel_1.add(lblCI);
+
+        JLabel lblCodigolibro = new JLabel("Codigo Libo:");
+        lblCodigolibro.setFont(new Font("Rockwell", Font.PLAIN, 18));
+        lblCodigolibro.setBounds(189, 172, 114, 14);
+        panel_1.add(lblCodigolibro);
+
+        JLabel lblFechareserva = new JLabel("Fecha reserva(dd/mm/yyyy):");
+        lblFechareserva.setFont(new Font("Rockwell", Font.BOLD, 18));
+        lblFechareserva.setBounds(36, 218, 281, 14);
+        panel_1.add(lblFechareserva);
+
+        JButton btnReservar = new JButton("Agregar reserva");
+        btnReservar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                int confirmacion = JOptionPane.showConfirmDialog(null, "¿Está seguro de registrar esta reserva?", "Confirmación de registro",
+                        JOptionPane.YES_NO_OPTION);
+                if (confirmacion == JOptionPane.YES_OPTION) {
+                    guardarDatosEnArchivo();
+
+
+                }
+            }
+        });
+        btnReservar.setFont(new Font("Rockwell", Font.PLAIN, 18));
+        btnReservar.setBounds(141, 309, 293, 31);
+        panel_1.add(btnReservar);
+
+        JButton btnNewButton = new JButton("Atrás");
+        btnNewButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JFMReservas frame = new JFMReservas();
+                frame.setVisible(true);
+                dispose();
+            }
+        });
+        btnNewButton.setBounds(829, 592, 89, 23);
+        contentPane.add(btnNewButton);
+    }
+    // Método para cargar usuarios desde el archivo
+    private List<Usuario> obtenerListaUsuariosDesdeArchivo(String nombreArchivo) {
+        List<Usuario> listaUsuario = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(nombreArchivo))) {
+            String linea;
+            while ((linea = reader.readLine()) != null) {
+                if (linea.startsWith("CI: ")) {
+                    int ci = Integer.parseInt(extraerValor(linea, "CI: "));
+                    String nombre = extraerValor(reader.readLine(), "Nombre: ");
+                    int celular = Integer.parseInt(extraerValor(reader.readLine(), "Celular: "));
+
+                    Usuario usuario = new Usuario(ci, nombre, celular);
+                    listaUsuario.add(usuario);
+
+                    reader.readLine(); // Leer la línea de separación
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return listaUsuario;
+    }
+    private String extraerValor(String linea, String etiqueta) {
+        return linea.substring(etiqueta.length()).trim();
+    }
+
+    // Método para cargar libros desde el archivo
+    private List<Libro> obtenerListaLibrosDesdeArchivo(String nombreArchivo) {
+        List<Libro> listaLibros = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(nombreArchivo))) {
+            String linea;
+            while ((linea = reader.readLine()) != null) {
+
+                String isbn = extraerValor(linea, "ISBN:");
+                String titulo = extraerValor(reader.readLine(), "Título:");
+                String autor = extraerValor(reader.readLine(), "Autor:");
+                String genero = extraerValor(reader.readLine(), "Género:");
+                int anioPublicacion = Integer.parseInt(extraerValor(reader.readLine(), "Año de Publicación:"));
+                int cantidad = Integer.parseInt(extraerValor(reader.readLine(), "Cantidad:"));
+                String rutaImagen = extraerValor(reader.readLine(), "Ruta de la Imagen:");
+
+                Libro libro = new Libro(isbn, titulo, autor, genero, anioPublicacion, cantidad, rutaImagen);
+                listaLibros.add(libro);
+                reader.readLine();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return listaLibros;
+    }
+    // Método para buscar un usuario por CI
+    private Usuario buscarUsuarioPorCI(int ci) {
+        for (Usuario usuario : usuariosRegistrados) {
+            if (usuario.getCI() == ci) {
+                return usuario;
+            }
+        }
+        return null; // No se encontró el usuario
+    }
+
+    // Método para buscar un libro por código
+    private Libro buscarLibroPorISBN(String codigoLibro) {
+        for (Libro libro : librosRegistrados) {
+            if (libro.getIsbn().equals(codigoLibro)) {
+                return libro;
+            }
+        }
+        return null; // No se encontró el libro
+    }
+    private boolean validarFechas(String fechareserva) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        dateFormat.setLenient(false);
+
+        try {
+            Date fechaReservaDate = dateFormat.parse(fechareserva);
+
+        } catch (ParseException e) {
+            JOptionPane.showMessageDialog(null, "Formato de fecha incorrecto. Utilice el formato dd/MM/yyyy.");
+            return false;
+        }
+
+        return true;
+    }
+    private int obtenerId(String nombreArchivo){
+        int id = 1;
+        try (BufferedReader reader = new BufferedReader(new FileReader(nombreArchivo))) {
+            String linea;
+            while ((linea = reader.readLine()) != null) {
+                if (linea.startsWith("PrID: ")) {
+                    id++;
+                    reader.readLine(); // Leer la línea de separación
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return id;
+    }
+    
+    private void guardarDatosEnArchivo() {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("Reservas.txt", true))) {
+            int prId = obtenerId("Reservas.txt");
+            int ci = Integer.parseInt(txCI.getText());
+            String codigoLibro = txCodigoLibro.getText();
+            String fechareserva = txFechareserva.getText();
+
+            Usuario usuario = buscarUsuarioPorCI(Integer.parseInt(txCI.getText()));
+            Libro libro = buscarLibroPorISBN(txCodigoLibro.getText());
+
+            if (!validarFechas(fechareserva)) {
+                return; // Si las fechas no son válidas, salir sin guardar
+            }
+            if (usuario != null && libro != null) {
+            	Reserva reserva = new Reserva(prId, usuario, libro, fechareserva);
+                
+                writer.write("PrID: " + prId + "\n");
+                writer.write("CI: " + ci + "\n");
+                writer.write("Codigo Libro: " + codigoLibro + "\n");
+                writer.write("Fecha reserva: " + fechareserva + "\n");
+                writer.write("------------------------------\n");
+
+                JOptionPane.showMessageDialog(null, "Reserva registrada.");
+                limpiarCampos();
+            } else {
+                JOptionPane.showMessageDialog(null, "Usuario o libro no registrado. No se puede realizar la reserva.");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private void limpiarCampos() {
+        txCI.setText("");
+        txCodigoLibro.setText("");
+        txFechareserva.setText("");
+    }
+
+}
